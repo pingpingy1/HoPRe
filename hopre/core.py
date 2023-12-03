097ce8362d5aad1c81adab604c3833a4fc75d8b9
pingpingy1 <pingpingy@kaist.ac.kr>
2023-12-03 21:52:33 +0900

    [fix] Change englishAnalyzer file to lambda

--- hopre/core.py
+++ hopre/core.py
@@ -13,15 +13,13 @@ def hopre() -> None:
 
     with PrologMQI() as mqi:
         with mqi.create_thread() as pthread:
-            english_analyzer = os.path.join(
-                BASE_DIR, "englishAnalysis", "englishAnalyzer.pl"
-            )
+            english_analyzer = os.path.join(BASE_DIR, "englishAnalysis", "lambda.pl")
             print(english_analyzer)
             pthread.query(f'consult("{english_analyzer}")')
 
             while True:
                 os.system("clear")
-                pthread.query_async("analyzeSentence")
+                pthread.query_async("lambda")
                 print(pthread.query_async_result())
 
                 if input("Press enter to continue or q to exit...") == "q":
